2ba8588a60c0a82d1c317462a3d4ae871a576476
njorogepeter <pgakonde@yahoo.com>
2019-04-19 12:33:37 -0700

    working on devour

--- db/schema.sql
+++ db/schema.sql
@@ -4,7 +4,8 @@ USE burgers_db;
 DROP TABLE IF EXISTS burgers;
 
 CREATE TABLE burgers (
-id int NOT NULL AUTO_INCREMENT PRIMARY KEY,
+id int NOT NULL AUTO_INCREMENT,
 burger_name varchar(255) NOT NULL,
-devoured BOOL DEFAULT false,
+devoured BOOLEAN DEFAULT false,
+PRIMARY KEY
 );
